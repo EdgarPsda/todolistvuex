1feef221b9d0229dd3def9cec6053ad07f93f337
Edgar Posada <edgar.posada@grupored.com.mx>
2019-09-13 17:15:38 -0700

    WIP: Store user's todo * Working on store modal.

--- resources/js/views/todos/index.vue
+++ resources/js/views/todos/index.vue
@@ -3,7 +3,9 @@
     <div class="row">
       <div class="col-md-12">
         <div class="panel panel-primary">
-          <div class="panel-heading"><h3>My To-Do List</h3></div>
+          <div class="panel-heading">
+            <h3>My To-Do List</h3>
+          </div>
           <table class="table table-striped">
             <thead>
               <tr>
@@ -19,29 +21,45 @@
                 <td>{{ todo.description }}</td>
                 <td>{{ todo.dueDate }}</td>
                 <td>
-                    <div class="btn-group btn-group-sm">
-                        <button class="btn btn-warning"><li class="fa fa-book"></li></button>
-                        <button class="btn btn-danger"><li class="fa fa-trash"></li></button>
-                    </div>
+                  <div class="btn-group btn-group-sm">
+                    <button class="btn btn-warning">
+                      <li class="fa fa-book"></li>
+                    </button>
+                    <button class="btn btn-danger">
+                      <li class="fa fa-trash"></li>
+                    </button>
+                  </div>
                 </td>
               </tr>
             </tbody>
           </table>
           <div class="panel-footer">
-            <button class="btn btn-primary">Add ToDo</button>
+            <button class="btn btn-primary" @click="openModal('post')">Add ToDo</button>
           </div>
         </div>
       </div>
     </div>
+
+    <!-- Modal -->
+    <modal :options="modal">
+
+    </modal>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import Modal from "../../components/Modal";
 
 export default {
   data() {
-    return {};
+    return {
+      modal:{
+        title: "",
+        btnClass: "",
+        btnTitle: ""
+      }
+    };
   },
   props: {
     user: {
@@ -58,7 +76,22 @@ export default {
   },
 
   methods: {
-    ...mapActions("Todo", ["fetchData"])
+    ...mapActions("Todo", ["fetchData"]),
+
+    openModal(action){
+      if(action === "post"){
+        this.modal.title = "Create a To-Do";
+        this.modal.btnClass = "btn-primary";
+        this.modal.btnTitle = "Save";
+
+      }
+
+      $("#modal").modal("show");
+    },
+  },
+
+  components:{
+    Modal
   }
 };
 </script>
